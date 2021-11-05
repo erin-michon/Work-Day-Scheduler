1f7ad4dcaf329a41534cb6105c53dd4210d6e0c0
Erin M <erin.michon@gmail.com>
2021-11-05 17:10:28 -0400

    added dynamic coloring based on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 /*
 
 ACCEPTANCE CRITERIA
-WHEN I scroll down
-THEN I am presented with time blocks for standard business hours
 WHEN I view the time blocks for that day
 THEN each time block is color-coded to indicate whether it is in the past, present, or future
 WHEN I click into a time block
@@ -16,6 +14,7 @@ THEN the saved events persist
 
 // Define any variables and html elements that need to be pulled/manipulated
 let currentDay = document.querySelector("#currentDay");
+let timeblock = document.querySelectorAll("[data-hour]");
 
 //On page load, the current date is diplayed in the jumbotron
 let showDate = function() {
@@ -27,8 +26,45 @@ let showDate = function() {
     currentDay.textContent = today;
 };
 
+//Create a function that checks the current time against the timeblock time to determine the background color
+let timeblockColor = function () {
+    //Get current time
+    let currentHour = moment().hour();
+    console.log(currentHour);
+    
+    console.log(timeblock);
+
+    for (let i = 0; i < timeblock.length; i++) {
+        // console.log(timeblock[i].dataset.hour);
+        
+        if (timeblock[i].dataset.hour == currentHour) {
+            console.log("this is the current time");
+            timeblock[i].classList.add("present");
+        }
+        else if (timeblock[i].dataset.hour > currentHour) {
+            console.log("this is in the future");
+            timeblock[i].classList.add("future");
+        }
+        else if (timeblock[i].dataset.hour < currentHour) {
+            console.log("this is in the past");
+            timeblock[i].classList.add("past");
+        };
+
+    };
+
+    //Compare current hour to the timeblock hour
+    // for (let index = 0; index < array.length; index++) {
+    //     const element = array[index];
+        
+    //     if("data-hour") {
+
+    //     }
+    // }
+};
+
 // Function Calls
 showDate();
+timeblockColor();
 
 
 
